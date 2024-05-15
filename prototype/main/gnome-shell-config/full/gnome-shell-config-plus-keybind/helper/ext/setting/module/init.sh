f2f518e89c22b208f8d6adde9f132e34b05ce577
samwhelp <samwhelp@users.noreply.github.com>
2024-05-15 08:21:39 +0800

    fix

---
+++ prototype/main/gnome-shell-config/full/gnome-shell-config-plus-keybind/helper/ext/setting/module/init.sh
@@ -0,0 +1,59 @@
+
+
+################################################################################
+### Head: Setting / Module / Init
+##
+
+
+##
+## Load: Setting / Module / Dconf
+##
+
+. "${THE_MAIN_EXT_SETTING_MODULE_DIR_PATH}/system-dconf.sh"
+
+
+##
+## Load: Setting / Module / Gsettings
+##
+
+. "${THE_MAIN_EXT_SETTING_MODULE_DIR_PATH}/system-gsettings.sh"
+
+
+##
+## Load: Setting / Module / Gnome Shell / Config Main
+##
+
+. "${THE_MAIN_EXT_SETTING_MODULE_DIR_PATH}/gnome-shell-config-main.sh"
+
+
+##
+## Load: Setting / Module / Gnome Shell / Workspace Main
+##
+
+. "${THE_MAIN_EXT_SETTING_MODULE_DIR_PATH}/gnome-shell-workspace-main.sh"
+
+
+##
+## Load: Setting / Module / Gnome Shell / Extension / Ubuntu Dock
+##
+
+. "${THE_MAIN_EXT_SETTING_MODULE_DIR_PATH}/gnome-shell-extension-ubuntu-dock.sh"
+
+
+##
+## Load: Setting / Module / Gnome Shell / Keybind Main
+##
+
+. "${THE_MAIN_EXT_SETTING_MODULE_DIR_PATH}/gnome-shell-keybind-main.sh"
+
+
+##
+## Load: Setting / Module / Gnome-Shell / Keybind Custom
+##
+
+. "${THE_MAIN_EXT_SETTING_MODULE_DIR_PATH}/gnome-shell-keybind-custom.sh"
+
+
+##
+### Tail: Setting / Module / Init
+################################################################################
